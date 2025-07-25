29144000d5f913c8dc9f830136e95a1b43a7c1ba
jroanhorse18 <jroanhorse@explora.us>
2025-07-24 21:22:17 -0600

    sample model

---
+++ sketch.js
@@ -0,0 +1,66 @@
+let classifier;
+let img;
+let prediction = "Drop an image to classify";
+
+// Load the model
+function preload() {
+  let modelURL = "https://teachablemachine.withgoogle.com/models/NhkA5RCVY/model.json";
+  classifier = ml5.imageClassifier(modelURL, modelLoaded);
+}
+
+function modelLoaded() {
+  console.log("✅ Model loaded");
+}
+
+function setup() {
+  let canvas = createCanvas(400, 400);
+  canvas.drop(handleFile);
+  background(220);
+  textAlign(CENTER, CENTER);
+  textSize(16);
+  fill(0);
+  text(prediction, width / 2, height / 2);
+  textLeading(20);
+}
+
+function handleFile(file) {
+  if (file.type === 'image') {
+    loadImage(file.data, loadedImage => {
+      img = loadedImage;
+      image(img, 0, 0, width, height);
+      classifyImage(img);
+    });
+  } else {
+    prediction = "Please drop an image file.";
+  }
+}
+
+function classifyImage(image) {
+  classifier.classify(image, (results) => {
+    if (results && results.length >= 3 ) {
+      console.log("✅ Results:", results);
+      prediction = results[0].label + " (" + nf(results[0].confidence * 100, 2, 1) + "%)";
+    } 
+    else {
+      console.warn("⚠️ No results returned:", results);
+      prediction = "❌ No results returned";
+    }
+  });
+}
+
+
+function draw() {
+  background(220);
+
+  if (img) image(img, 0, 0, width, height);
+
+  fill(0);
+  noStroke();
+  rect(0, height - 30, width, 30);
+
+  fill(255);
+  textAlign(CENTER, CENTER);
+  text(prediction, width / 2, height - 15);
+}
+
+
